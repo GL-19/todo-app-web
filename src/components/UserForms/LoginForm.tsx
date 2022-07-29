72f981bc91d78146f794175f45a66868d9c770d1
GL-19 <loiolagl19@gmail.com>
2022-07-29 17:47:22 -0300

    feat: add validation to LoginForm

--- src/components/UserForms/LoginForm.tsx
+++ src/components/UserForms/LoginForm.tsx
@@ -1,20 +1,36 @@
 import { useState } from "react";
-import { useAuth } from "../../providers/AuthProvider";
 import { useForm } from "react-hook-form";
-import { FormContainer, Input, SubmitButton, Label } from "./components";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
-interface ILoginFormData {
+import { useAuth } from "../../providers/AuthProvider";
+import { FormContainer, Input, SubmitButton, Label, ErrorMsg } from "./components";
+
+interface ILoginFormInputs {
 	email: string;
 	password: string;
 }
 
+const LoginSchema = yup
+	.object({
+		email: yup.string().email("Invalid Email").required("Email is required"),
+		password: yup.string().required("Password is Required"),
+	})
+	.required();
+
 export function LoginForm() {
 	const { handleLogin } = useAuth();
-	const { register, handleSubmit } = useForm<ILoginFormData>();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<ILoginFormInputs>({
+		resolver: yupResolver(LoginSchema),
+	});
 
 	const [error, setError] = useState(false);
 
-	async function onSubmit(data: ILoginFormData) {
+	async function onSubmit(data: ILoginFormInputs) {
 		try {
 			setError(false);
 
@@ -27,12 +43,14 @@ export function LoginForm() {
 	return (
 		<FormContainer onSubmit={handleSubmit(onSubmit)}>
 			<Label>Email: </Label>
-			<Input placeholder="Email" type="email" {...register("email")} />
+			<Input placeholder="Email" {...register("email")} />
+			<ErrorMsg>{errors.email?.message}</ErrorMsg>
 
 			<Label>Password: </Label>
 			<Input placeholder="Password" type="password" {...register("password")} />
+			<ErrorMsg>{errors.password?.message}</ErrorMsg>
 
-			{error && <p>Email or Password incorrect!</p>}
+			{error && <ErrorMsg>Email or Password incorrect!</ErrorMsg>}
 			<SubmitButton type="submit">Login</SubmitButton>
 		</FormContainer>
 	);
